574b9df917dfa9ba6fdbe433ce4bb4f605ac8ecd
HTY2003 <randumbperson2003@gmail.com>
2018-04-21 21:34:50 +0800

    Files added

---
+++ src/qt/moc_chatwindow.cpp
@@ -0,0 +1,164 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'chatwindow.h'
+**
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "qt/chatwindow.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'chatwindow.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.5.1. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+struct qt_meta_stringdata_ChatWindow_t {
+    QByteArrayData data[12];
+    char stringdata0[123];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_ChatWindow_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_ChatWindow_t qt_meta_stringdata_ChatWindow = {
+    {
+QT_MOC_LITERAL(0, 0, 10), // "ChatWindow"
+QT_MOC_LITERAL(1, 11, 9), // "changeTab"
+QT_MOC_LITERAL(2, 21, 0), // ""
+QT_MOC_LITERAL(3, 22, 12), // "sendCommande"
+QT_MOC_LITERAL(4, 35, 8), // "connecte"
+QT_MOC_LITERAL(5, 44, 8), // "closeTab"
+QT_MOC_LITERAL(6, 53, 10), // "tabChanged"
+QT_MOC_LITERAL(7, 64, 5), // "index"
+QT_MOC_LITERAL(8, 70, 9), // "tabJoined"
+QT_MOC_LITERAL(9, 80, 10), // "tabJoining"
+QT_MOC_LITERAL(10, 91, 20), // "disconnectFromServer"
+QT_MOC_LITERAL(11, 112, 10) // "tabClosing"
+
+    },
+    "ChatWindow\0changeTab\0\0sendCommande\0"
+    "connecte\0closeTab\0tabChanged\0index\0"
+    "tabJoined\0tabJoining\0disconnectFromServer\0"
+    "tabClosing"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_ChatWindow[] = {
+
+ // content:
+       7,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       9,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       1,       // signalCount
+
+ // signals: name, argc, parameters, tag, flags
+       1,    0,   59,    2, 0x06 /* Public */,
+
+ // slots: name, argc, parameters, tag, flags
+       3,    0,   60,    2, 0x0a /* Public */,
+       4,    0,   61,    2, 0x0a /* Public */,
+       5,    0,   62,    2, 0x0a /* Public */,
+       6,    1,   63,    2, 0x0a /* Public */,
+       8,    0,   66,    2, 0x0a /* Public */,
+       9,    0,   67,    2, 0x0a /* Public */,
+      10,    0,   68,    2, 0x0a /* Public */,
+      11,    1,   69,    2, 0x0a /* Public */,
+
+ // signals: parameters
+    QMetaType::Void,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Int,    7,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Int,    7,
+
+       0        // eod
+};
+
+void ChatWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        ChatWindow *_t = static_cast<ChatWindow *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->changeTab(); break;
+        case 1: _t->sendCommande(); break;
+        case 2: _t->connecte(); break;
+        case 3: _t->closeTab(); break;
+        case 4: _t->tabChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 5: _t->tabJoined(); break;
+        case 6: _t->tabJoining(); break;
+        case 7: _t->disconnectFromServer(); break;
+        case 8: _t->tabClosing((*reinterpret_cast< int(*)>(_a[1]))); break;
+        default: ;
+        }
+    } else if (_c == QMetaObject::IndexOfMethod) {
+        int *result = reinterpret_cast<int *>(_a[0]);
+        void **func = reinterpret_cast<void **>(_a[1]);
+        {
+            typedef void (ChatWindow::*_t)();
+            if (*reinterpret_cast<_t *>(func) == static_cast<_t>(&ChatWindow::changeTab)) {
+                *result = 0;
+            }
+        }
+    }
+}
+
+const QMetaObject ChatWindow::staticMetaObject = {
+    { &QWidget::staticMetaObject, qt_meta_stringdata_ChatWindow.data,
+      qt_meta_data_ChatWindow,  qt_static_metacall, Q_NULLPTR, Q_NULLPTR}
+};
+
+
+const QMetaObject *ChatWindow::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *ChatWindow::qt_metacast(const char *_clname)
+{
+    if (!_clname) return Q_NULLPTR;
+    if (!strcmp(_clname, qt_meta_stringdata_ChatWindow.stringdata0))
+        return static_cast<void*>(const_cast< ChatWindow*>(this));
+    return QWidget::qt_metacast(_clname);
+}
+
+int ChatWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QWidget::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 9)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 9;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 9)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 9;
+    }
+    return _id;
+}
+
+// SIGNAL 0
+void ChatWindow::changeTab()
+{
+    QMetaObject::activate(this, &staticMetaObject, 0, Q_NULLPTR);
+}
+QT_END_MOC_NAMESPACE
